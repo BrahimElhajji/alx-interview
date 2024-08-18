55bc5cd4a9069360fa72d229ae4d0d12a4a3543d
BrahimElhajji <elhajji080898@gmail.com>
2024-08-18 12:45:57 -0700

    fixing log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,11 @@ log parsing
 
 import sys
 import re
-import signal
 
 
 def output(log: dict) -> None:
     """
-    helper function to display stats
+    Helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
@@ -18,41 +17,36 @@ def output(log: dict) -> None:
             print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-def signal_handler(sig, frame):
-    output(log)
-    sys.exit(0)
-
-
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-
     regex = re.compile(
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
     line_count = 0
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
-            str(code): 0 for code in [
-                200, 301, 400, 401, 403, 404, 405, 500]}
+        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
+    }
 
     try:
         for line in sys.stdin:
             line = line.strip()
-    match = regex.fullmatch(line)
-    if (match):
-        line_count += 1
-        code = match.group(1)
-        file_size = int(match.group(2))
+            match = regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-        # File size
-        log["file_size"] += file_size
+                # Update file size
+                log["file_size"] += file_size
 
-        # status code
-        if (code.isdecimal()):
-            log["code_frequency"][code] += 1
+                # Update status code frequency
+                if code.isdecimal():
+                    log["code_frequency"][code] += 1
 
-        if (line_count % 10 == 0):
-            output(log)
+                # Output stats every 10 lines
+                if line_count % 10 == 0:
+                    output(log)
     finally:
+        # Output final stats
         output(log)
